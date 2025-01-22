127fa3573ae6c5bd56f1be693c1bbb8260738468
saidmsaid <69822866+saidmsaid@users.noreply.github.com>
2025-01-22 12:19:24 +0300

    Add App.jsx component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import QatarMallsDirectory from './components/QatarMallsDirectory';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8 px-4">
+      <QatarMallsDirectory />
+    </div>
+  );
+}
+
+export default App;
